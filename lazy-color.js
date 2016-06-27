89e06c266fa47108451ed3c458534a7fc8efa718
Kunall Banerjee <yeskunall96@gmail.com>
2016-06-26 22:30:11 -0400

    lazy.init(); :grin:

--- lazy-color.js
+++ lazy-color.js
@@ -1,11 +1,23 @@
+/* jshint esversion: 6 */
+
 names = [
     ["cd5c5c", "IndianRed"],
     ["f08080", "LightCoral"],
     ["fa8072", "Salmon"],
     ["e9967a", "DarkSalmon"],
     ["ffa07a", "LightSalmon"],
-    ["dc143c","Crimson"],
-    ["ff0000","Red"],
-    ["b22222","Firebrick"],
+    ["dc143c", "Crimson"],
+    ["ff0000", "Red"],
+    ["b22222", "Firebrick"],
     ["8b0000", "DarkRed"]
 ];
+
+var lazy = {
+    init: function() {
+        let index;
+        index = Math.floor(names.length * Math.random());
+        console.log(names[index]);
+    },
+};
+
+lazy.init();
