7f17d9668b4bc4e1bfa7221624bfabfcb4c366fb
Kunall Banerjee <yeskunall96@gmail.com>
2016-06-26 21:55:07 -0400

    Added all HTML names for RED

---
+++ lazy-color.js
@@ -0,0 +1,11 @@
+names = [
+    ["cd5c5c", "IndianRed"],
+    ["f08080", "LightCoral"],
+    ["fa8072", "Salmon"],
+    ["e9967a", "DarkSalmon"],
+    ["ffa07a", "LightSalmon"],
+    ["dc143c","Crimson"],
+    ["ff0000","Red"],
+    ["b22222","Firebrick"],
+    ["8b0000", "DarkRed"]
+];
