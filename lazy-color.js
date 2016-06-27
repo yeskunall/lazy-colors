e4f1c31bcf3dbc8613506f181cff1dd363b56f79
Kunall Banerjee <yeskunall96@gmail.com>
2016-06-26 22:37:38 -0400

    Added all HTML names for PINK

--- lazy-color.js
+++ lazy-color.js
@@ -1,6 +1,7 @@
 /* jshint esversion: 6 */
 
 names = [
+    // RED
     ["cd5c5c", "IndianRed"],
     ["f08080", "LightCoral"],
     ["fa8072", "Salmon"],
@@ -9,14 +10,21 @@ names = [
     ["dc143c", "Crimson"],
     ["ff0000", "Red"],
     ["b22222", "Firebrick"],
-    ["8b0000", "DarkRed"]
+    ["8b0000", "DarkRed"],
+    // PINK
+    ["ffc0cb", "Pink"],
+    ["ffb6c1", "LightPink"],
+    ["ff69b4", "HotPink"],
+    ["ff1493", "DeepPink"],
+    ["c71585", "MediumVioletRed"],
+    ["db7093", "PaleVioltedRed"],
 ];
 
 var lazy = {
     init: function() {
         let index;
         index = Math.floor(names.length * Math.random());
-        console.log(names[index]);
+        return names[index];
     },
 };
 
