bd3f6c4d2cf794196309b03cd7b1998cac4272db
Kunall Banerjee <yeskunall96@gmail.com>
2016-06-28 10:55:14 -0400

    Fix issue #1

--- public/js/lazy-color.js
+++ public/js/lazy-color.js
@@ -3,8 +3,6 @@
 $(document).ready(function() {
 
     // when the browser loads the site for the first time
-    // need to add something here
-
     $('.scss').hide();
     $('.variable').hide();
 
@@ -193,13 +191,12 @@ var lazy = {
         return names[Math.floor(names.length * Math.random())];
     },
 
-    getColorContrast: function(hexcolor){
+    getColorContrast: function(hexcolor) {
         hexcolor = hexcolor.replace('#', '');
-        var r = parseInt(hexcolor.substr(0,2),16);
-        var g = parseInt(hexcolor.substr(2,2),16);
-        var b = parseInt(hexcolor.substr(4,2),16);
-        var yiq = ((r*299)+(g*587)+(b*114))/1000;
-        console.log(yiq, hexcolor);
-        return (yiq >= 128) ? 'black' : 'white';
+        var r = parseInt(hexcolor.substr(0, 2), 16);
+        var g = parseInt(hexcolor.substr(2, 2), 16);
+        var b = parseInt(hexcolor.substr(4, 2), 16);
+        var yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+        return (yiq >= 128) ? '#353E55' : '#F0F5F9';
     }
 };
